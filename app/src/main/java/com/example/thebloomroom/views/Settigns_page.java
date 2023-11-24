62c808dd3af2e8b730b3d55c8bbd67cad30b0164
Anushka lakshan <anushkalakshan1996@gmail.com>
2023-11-24 23:33:15 +0530

    finished

--- app/src/main/java/com/example/thebloomroom/views/Settigns_page.java
+++ app/src/main/java/com/example/thebloomroom/views/Settigns_page.java
@@ -2,15 +2,47 @@ package com.example.thebloomroom.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.thebloomroom.R;
 
+
 public class Settigns_page extends AppCompatActivity {
+
+    int UserID;
+    String UserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settigns_page);
+
+        UserID = getIntent().getIntExtra("UserID", 0);
+
+        UserName = getIntent().getStringExtra("user");
+    }
+
+    public void Logout(View view){
+        Intent intent = new Intent(this, Login.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+
+    public void gotoCart(View view){
+        Intent intent = new Intent(this, Cart_page.class);
+        intent.putExtra("UserID", this.UserID);
+        intent.putExtra("user", UserName);
+        startActivity(intent);
+    }
+
+    public void gotoMain(View view){
+        Intent intent = new Intent(this, Main_page.class);
+        intent.putExtra("user_id", UserID);
+        intent.putExtra("user", UserName);
+        startActivity(intent);
     }
 }
